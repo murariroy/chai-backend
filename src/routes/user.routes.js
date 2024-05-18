ed56837812656ade91f09d565f65c97e2e7844ef
murariroy <murarizf@gmail.com>
2024-05-18 19:16:00 +0530

    add new router for user

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,12 @@
 import { Router } from "express";
 import { registerUser,logoutUser,
-    registerUser,refreshAccessToken
+    registerUser,refreshAccessToken,
+    getCurrentUser,
+    updateAccountDetails,
+    updateUserAvatar,
+    updateUserCoverImage,
+    getUserChannelProfile,
+    getWatchhistory
 } from "../controllers/user.controller.js";
 
 import {upload} from "../middlewares/multer.middleware.js"
@@ -29,6 +35,19 @@ router.route("/register").post(
 
     router.route("/refresh-token").post(refreshAccessToken)
     
+
+    router.route("/change-password").post(verifyJWT,
+        changeCurrentPassword)
+        router.route("/current-user".length(verifyJWT,getCurrentUser))
+        router.route("/update-account").patch(verifyJWT,updateAccountDetails)
+
+        router.route("/avatar").patch(verifyJWT,upload.single("avatar"),updateUserAvatar)
+
+        router.route("/cover-image").patch(verifyJWT,upload.single("/coverImage"),updateUserCoverImage)
+        router.route("/c/:username").get(verifyJWT,getUserChannelProfile)
+
+        router.route("/history").get(verifyJWT,getWatchhistory)
+
 
 
 export default router;
